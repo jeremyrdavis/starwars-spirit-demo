85ec065e2c5cab7d2c596918a6c543a43d5d79bd
jeremyrdavis <jeremyrdavis@mac.com>
2024-03-05 13:01:15 -0500

    added new project

--- swapi-client/src/main/java/io/arrogantprogrammer/GreetingResource.java
+++ swapi-client/src/main/java/io/arrogantprogrammer/GreetingResource.java
@@ -11,14 +11,16 @@ import org.eclipse.microprofile.rest.client.inject.RestClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.HashMap;
 import java.util.Random;
 
 @Path("/hello")
 public class GreetingResource {
+    static final Logger LOGGER = LoggerFactory.getLogger(GreetingResource.class);
 
     private static int count;
 
-    static final Logger LOGGER = LoggerFactory.getLogger(GreetingResource.class);
+    private static HashMap<Integer, StarWarsCharacter> characters = new HashMap<>();
 
     @RestClient
     SwapiClient swapiClient;
@@ -33,10 +35,24 @@ public class GreetingResource {
     @Path("/spiritcharacter")
     @Produces(MediaType.APPLICATION_JSON)
     public StarWarsCharacter getCharacter() {
-
         if (count == 0) {
             count = swapiClient.getAllCharacters().count();
         }
-        return swapiClient.getCharacter( new Random().nextInt(count - 1) + 1);
+        if(characters.containsKey(count)){
+            return characters.get(count);
+        }else {
+            StarWarsCharacter starWarsCharacter = swapiClient.getCharacter( new Random().nextInt(count - 1) + 1);
+            characters.put(count, starWarsCharacter);
+            return starWarsCharacter;
+        }
+    }
+
+    @GET
+    @Path("/charactercount")
+    public int getCharacterCount() {
+        if (count == 0) {
+            count = swapiClient.getAllCharacters().count();
+        }
+        return count;
     }
 }
